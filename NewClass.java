4c69d31e93876d0268d525798241b08075e1d34f
Mamatadhake <118421815+Mamatadhake@users.noreply.github.com>
2024-02-02 15:20:29 +0530

    Add files via upload
    Mamta Updated

---
+++ NewClass.java
@@ -0,0 +1,10 @@
+package com.JenkinUse.JenkinMavenProject;
+
+public class NewClass {
+	
+	public static void main(String []args) {
+		
+		System.out.println("this is my first jenkins progrsm");
+	}
+
+}
